470db1d3eb602e3e541e30e2c639e4b4445712ab
Stan <Stanbankras@gmail.com>
2020-03-26 13:30:13 +0100

    Basic setup is ready to start building in branches

---
+++ routes/chatting.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const mongo = require('mongodb');
+const ObjectID = mongo.ObjectID;
+// Use database connection from server.js
+const dbCallback = require('../server.js').db;
+let db;
+dbCallback(database => {
+  db = database
+});
+
+router.get('/chatting', async (req, res, next) => {
+  try {
+    const user = await db.collection('users').findOne({ 'firstName': 'Jan' });
+    console.log(user);
+    res.render('test');
+  } catch(err) {
+    console.log(err);
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const mongo = require('mongodb');
+
+// Load environment variables
+require('dotenv').config();
+
+// Mongo setup code, obtained from the Full Driver Sample provided by MongoDB
+let db = null;
+const callbacks = [];
+const MongoClient = mongo.MongoClient;
+const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
 
+// Establish connection to MongoDB database
+client.connect(() => {
+  db = client.db(process.env.DB_NAME);
+  callbacks.forEach(callback => callback(db));
+  server.listen(port);
+  server.on('listening', () => console.log('Listening on port ' + port));
+});
+
+// Received help from Alex Bankras (Software engineer) to be able to export my database connection & server
+module.exports = {
+  db: function(cb) {
+      if(db) {
+          cb(db)
+      } else {
+          callbacks.push(cb);
+      }
+  }
+}
+
+const likeRouter = require('./routes/liking');
+const chatRouter = require('./routes/chatting');
+const loginRouter = require('./routes/login');
+const matchingRouter = require('./routes/matching');
+app.use('/', likeRouter); // Liking routes
+app.use('/', chatRouter); // Chatting routes
+app.use('/', loginRouter); // Chatting routes
+app.use('/', matchingRouter); // Matching routes
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req,res) => { res.status(404).render('404.ejs'); }); // 404 route
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
-
-app.get('/', (req, res, next) => {
-  res.render('test');
-})
-
-app.listen(port, () => console.log(`Server is running succesfully on port ${ port }`));
