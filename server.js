31374a4112583d2a0f1a00aa7a4b4477f7d676fd
Stan <Stanbankras@gmail.com>
2020-03-28 13:34:36 +0100

    Changed port variables so Heroku can set the port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWO
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 // Establish connection to MongoDB database
