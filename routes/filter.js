6dd056e5db536307de4cd75d12de0ccd1f0ec7f4
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-04-09 16:49:42 +0200

    filtet buttons code simplefied and hobbies are now able to be loaded in from the database into the buttons

--- routes/filter.js
+++ routes/filter.js
@@ -4,43 +4,36 @@ const router = express.Router();
 const dbCallback = require('../server.js').db;
 let db;
 dbCallback(database => {
-  db = database
+  db = database;
 });
 
-router.get('/finder', function(req,res){
-  let hob = req.session.hobby1 
-  if(hob) {
-      db.collection('users').find({'hobby1': hob}).toArray(done)
-      function done(err) {
-           if (err) {
-              next(err)
-           } else {
-              res.redirect('/result')
-           }
-       }
-  }
-  else {
-    const route = 'finder';
-    res.render('pages/filter/filter.ejs', { route });
-  }
-})
+router.get('/finder', async (req,res) =>{
+  const hobbies = await db.collection('hobbies').find().toArray();
+  const route = 'finder';
+    res.render('pages/filter/filter.ejs', { route, hobbies });
+  })
 
 router.post('/result', async (req, res) => {
   try{
     if (req.body.hobby1) {
-      req.session.hobby1 = req.body.hobby1
+      req.session.hobby1 = req.body.hobby1;
     }
-    let hob = req.session.hobby1 
+    let hob = req.session.hobby1;
     if(hob) { 
-      const data = await db.collection('users').find({'hobby1': hob}).toArray()
-      res.render('pages/filter/result.ejs', {data: data})
+      const data = await db.collection('users').find({'hobby1': hob}).toArray();
+      res.render('pages/filter/result.ejs', {data: data});
     } else {
-      res.redirect('/return')
+      res.redirect('/return');
     }
   } catch(err){
-    console.log(err)
+    console.log(err);
   }
 }) 
+
+// db.collection('users').find( { tags: { $all: [hobby] } } )
+// db.inventory.find( { tags: hob } )
+// zoek op hobby naam in de hobbies collection en krijg het id
+// en met dat id zoeken in het array van de user 
 
 router.get('/result', (req, res, next) => {
     let hob = req.session.hobby1
