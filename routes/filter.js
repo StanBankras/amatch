8c14470cbcabfc78104a682c7a5d0473da3d4a3f
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-04-11 18:13:06 +0200

    implementing new search funtion and trying to render the contents of 'hobbies' array by using the names from the hobbies collection

--- routes/filter.js
+++ routes/filter.js
@@ -7,58 +7,71 @@ dbCallback(database => {
   db = database;
 });
 
-router.get('/finder', async (req,res) =>{
+router.get('/finder', async (req, res) => {
   const hobbies = await db.collection('hobbies').find().toArray();
   const route = 'finder';
-    res.render('pages/filter/filter.ejs', { route, hobbies });
-  })
+  res.render('pages/filter/filter.ejs', { route, hobbies });
+})
+
+//pak de inhoud van het inputfield
+//zoek de hobby op en zet het om naar het id van die hobby
+//zoek in het array met hobbys van de gebruiker
+//array de gebruikers die dat hebben
 
 router.post('/result', async (req, res) => {
-  try{
-    if (req.body.hobby1) {
-      req.session.hobby1 = req.body.hobby1;
-    }
-    let hob = req.session.hobby1;
-    if(hob) { 
-      const data = await db.collection('users').find({'hobby1': hob}).toArray();
-      res.render('pages/filter/result.ejs', {data: data});
-    } else {
-      res.redirect('/return');
-    }
-  } catch(err){
+  try {
+    let filter = req.body.filter;
+    const hobbyName = await db.collection('hobbies').find({
+      name: filter
+    }).toArray();
+    let hobbyId = hobbyName._id;
+    const data = await db.collection('users').find({ tags: { $all: [hobbyId] } }).toArray();
+    const route = 'result';
+    console.log(hobbyId);
+    res.render('pages/filter/result.ejs', { data: data, route, hobbyName });
+  } catch (err) {
     console.log(err);
   }
-}) 
+})
+
+// { results: { $elemMatch: { _id: hobbyId} } }
+
+// res.render('pages/filter/result.ejs', { data: data, hobbies }
+// const data = await db.collection('users').find({ tags: { $all: [filter] } })
 
 // db.collection('users').find( { tags: { $all: [hobby] } } )
+// const hobbies = await db.collection('hobbies').find().toArray();
 // db.inventory.find( { tags: hob } )
 // zoek op hobby naam in de hobbies collection en krijg het id
 // en met dat id zoeken in het array van de user 
 
-router.get('/result', (req, res, next) => {
-    let hob = req.session.hobby1
-    if (hob) {
-       db.collection('users')
-           .find({'hobby1': hob}).toArray(done)
-    } else {
-       res.redirect('/return')
-    }
-    function done(err, data) {
-      if (err) {
-        next(err)
-      } else {
-         res.render('pages/filter/result.ejs', {data: data})
-      }
-    }
+
+//haal alle documents uit 'hobbies' op
+//
+
+router.get('/result', (req, res) => {
+  try {
+    const hobbyList = db.collection('users').find({ 
+      hobbies
+    });
+    const data = db.collection('hobbies').find( { 
+      _id: hobbyList 
+    });
+    let nameOfHobby = data.name;
+    res.render('pages/filter/result.ejs', { data: data, nameOfHobby});
+  } catch (err) {
+    console.log(err);
+    res.redirect('/return')
+  }
 })
 
-router.get('/return',function(req,res){
-  if (req.session.hobby1) {
-        req.session.destroy(function(err) {
-        if (err) console.log(err)
+router.get('/return', function (req, res) {
+  if (req.session.filter) {
+    req.session.destroy(function (err) {
+      if (err) console.log(err)
     })
-   }
-   res.redirect('/filter')
+  }
+  res.redirect('/filter')
 })
 
 module.exports = router;
