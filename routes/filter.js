ba413bf1f3b3f3e1eef20747f9ec83ad6c963555
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-04-12 22:43:34 +0200

    new filter feature working

--- routes/filter.js
+++ routes/filter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+// const ObjectID = require('mongodb').ObjectID;
 // Use database connection from server.js
 const dbCallback = require('../server.js').db;
 let db;
@@ -21,31 +22,18 @@ router.get('/finder', async (req, res) => {
 router.post('/result', async (req, res) => {
   try {
     let filter = req.body.filter;
-    const hobbyName = await db.collection('hobbies').find({'name': filter}).toArray();
-    hobbyName.forEach((hobbyName) => {
-      hobbyId = hobbyName._id;
-      hobbyId.toString();
-    });
+    const hobbyName = await db.collection('hobbies').findOne({'name': filter});
+    let hobbyId = hobbyName._id;
     console.log(hobbyId);
-    // const data = await db.collection('users').find({ hobbies: {$all: [hobbyId]}}).toArray();
-    const data = await db.collection('users').find({ hobbies: {$all: [hobbyId, hobbyId]}}).toArray();
+    const data = await db.collection('users').find({ hobbies: hobbyId.toHexString() }).toArray();
+    const route = 'result';
     console.log(data);
     console.log(hobbyId);
-    const route = 'result';
-    res.render('pages/filter/result.ejs', { data: data, hobbyName, route });
+    res.render('pages/filter/result.ejs', { data: data, hobbyName, hobbyId, route });
   } catch (err) {
     console.log(err);
   }
 })
-
-// { results: { $elemMatch: { _id: hobbyId} } }
-
-// res.render('pages/filter/result.ejs', { data: data, hobbies }
-// const data = await db.collection('users').find({ tags: { $all: [filter] } })
-
-// db.collection('users').find( { tags: { $all: [hobby] } } )
-// const hobbies = await db.collection('hobbies').find().toArray();
-// db.inventory.find( { tags: hob } )
 
 //haal alle items uit het array 'hobbies' op
 //vergelijk welke id's van het array 'hobbies' in 'hobbies' collection zitten
