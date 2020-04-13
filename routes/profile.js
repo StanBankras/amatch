010b5e65d4c5250636aeffdcf2e8b4f81727eaa5
mbergevoet <34296504+mbergevoet@users.noreply.github.com>
2020-04-13 17:16:07 +0200

    api request now able to be sent and that data is being rendered on the profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongo = require('mongodb');
 const auth = require('../middleware/authentication');
+const fetch = require('node-fetch');
 const profilePictureUpload = require('../helpers/multer').profilePicture;
 const ObjectID = mongo.ObjectID;
 // Use database connection from server.js
@@ -28,7 +29,11 @@ router.get('/profile/:id', auth, async (req, res) => {
     const activeUser = await db.collection('users').findOne({ _id: ObjectID(req.session.activeUser) });
     const hobbies = await db.collection('hobbies').find().toArray();
     const route = 'profile'
-    res.render('pages/profile-preview', { user, route, hobbies, activeUser });
+    artistId = user.deezerArtistId;
+    const apiUrl = 'https://api.deezer.com/artist/' + artistId;
+    const fetchResponse = await fetch(apiUrl);
+    const json = await fetchResponse.json();
+    res.render('pages/profile-preview', { user, route, hobbies, activeUser, json });
   } catch(err) {
     console.log(err);
   }
