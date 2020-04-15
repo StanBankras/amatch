b1cf8f9e445d1b071c18f28f2f71512d2904a2b6
HappyPantss <sergioeijben@hotmail.com>
2020-04-15 20:05:05 +0200

    Deezer Artist Id only showing if hobbies contain Music

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,5 +1,31 @@
-$('#submitButton').click(function() {
-    $('div.formpjes:nth-child(14)').css({
-        'display': 'none'
-    });
-});
+// Only show Deezer Artist ID select input if 'Music' is chosen as hobby.
+const deezerForm = document.querySelector('div#deezerArtistId');
+
+function showDeezer() {
+    document.querySelector('#selectHobbies').addEventListener('change', function (e) {
+        if (e.target.value === '5e8f0b1b34c66624801e0bb4') {
+            deezerForm.style.visibility = 'visible';
+        } else {
+            deezerForm.style.visibility = 'hidden';
+        }
+    })
+}
+
+showDeezer();
+
+// Check if confirmed password equals password
+const password = document.querySelector('#password');
+const cPassword = document.querySelector('#confirmPassword');
+
+function validatePassword() {
+    if (password.value == cPassword.value) {
+        cPassword.style.borderColor = 'green';
+        confirmPassword.setCustomValidity('');
+    } else {
+        cPassword.style.borderColor = '';
+        confirmPassword.setCustomValidity('Passwords don\'t match');
+    }
+}
+
+password.onchange = validatePassword;
+cPassword.onkeyup = validatePassword;
